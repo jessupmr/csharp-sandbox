2feac166f0d99173d5b9ed62a7533a278d50263c
Mike Jessup <Mike.Jessup@Honeywell.com>
2018-12-03 16:12:45 -0700

    WIP: just started the console writers.

---
+++ src/ConsoleUtilities/Writers/IWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleUtilities.Writers
+{
+	public interface IWriter
+	{
+		string HeaderLine { get; set; }
+		string SeparatorLine { get; set; }
+
+		void Character(string character);
+		void Line(string text);
+		void EmptyLine();
+		void Separator();
+		void Header(string text);
+	}
+}
---
+++ src/ConsoleUtilities/Writers/Writer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUtilities.Writers
+{
+	public class Writer : IWriter
+	{
+		#region Constructors
+		public Writer() : this("--------------------------------", "---------- ---------- ----------") { }
+
+		public Writer(string HeaderLine, string SeparatorLine)
+		{
+			this.HeaderLine = HeaderLine;
+			this.SeparatorLine = SeparatorLine;
+		}
+		#endregion
+
+		#region Properties
+		public string HeaderLine { get; set; }
+		public string SeparatorLine { get; set; }
+
+		public void Character(string character)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void EmptyLine()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Header(string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Line(string text)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Separator()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
